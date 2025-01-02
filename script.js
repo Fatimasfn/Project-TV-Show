a424c414b69d8a70754f589c59f54f489589728e
fatima safana <asksiyam@gmail.com>
2025-01-02 19:15:32 +0000

    implement a function that create episode code.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //You can edit ALL of the code here
 
-
 function setup() {
   const allEpisodes = getAllEpisodes();
   return makePageForEpisodes(allEpisodes);
@@ -13,25 +12,36 @@ function makePageForEpisodes(episodeList) {
   return rootElem;
 }
 
+//implement a function that create episode code.
+//1: gets season number, and episode number
+//2: pad the no. with zero and concatenate a string
+//e.g Season2 episode 7 should evaluate to S02E07
+function episodeCode(episode) {
+  // Extract season and episode numbers
+  const season = episode.season.toString().padStart(2, '0');
+  const number = episode.number.toString().padStart(2, '0');
+  // Concatenate the formatted string
+  return `S${season}E${number}`;
+}
+
+
+
 //getOneEpisode returns an obj with info for one episode.
 const episode = getOneEpisode();
 
 //create a function that fill in the tag and append it.
-
 const createEpisodeCard = (episode) =>{
 
 const episodeCard = document.getElementById("episode-card").content.cloneNode(true);
 episodeCard.querySelector("h3").textContent = episode.name;
-episodeCard.querySelector("#episode-code").textContent = episode.season, episode.number;
+episodeCard.querySelector("#episode-code").textContent = `${episodeCode(episode)}`;
 episodeCard.querySelector("img").src = episode.image.medium;
-episodeCard.querySelector("#summary").textContent = episode.summary;
+episodeCard.querySelector("#summary").innerHTML = episode.summary;
 
-return episodeCard
+return episodeCard;
 }
 
-
 const episodeCard = createEpisodeCard(episode);
-
 document.body.append(episodeCard);
 
 
